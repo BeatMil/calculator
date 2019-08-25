6a5813f9b9e22e61bc706f6ea373b2a4a1c0852c
BeatMil <anutorn@gmail.com>
2019-08-25 19:44:42 +0700

    trying to pass variable from class to another class

--- lib/cal.dart
+++ lib/cal.dart
@@ -7,9 +7,14 @@ class Calculator extends StatefulWidget {
   }
 }
 
-class CalculatorState extends State<Calculator> {
+class  CalculatorState extends State<Calculator> {
 
   int _numDisplay = 0;
+  int _buttonCount = 0;
+
+  int getButtonCount() {
+    return _buttonCount;
+  }
 
   Widget _rowNum(int number) {
     return Row(
@@ -18,6 +23,10 @@ class CalculatorState extends State<Calculator> {
           onPressed: (){
             setState(() {
              _numDisplay = number;
+             _buttonCount++;
+             print(_buttonCount);
+            //  _StatState()._buttonCount = this._buttonCount;
+            //  _StatState().didUpdateWidget(Stat());
             });
           },
           child: Text(number.toString(),
@@ -26,7 +35,6 @@ class CalculatorState extends State<Calculator> {
       ],
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -71,11 +79,70 @@ class CalculatorState extends State<Calculator> {
               _rowNum(2),
               _rowNum(3),
             ],
+          ),
+          FloatingActionButton(
+            child: Text('Count'),
+            onPressed: () {
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context) => Stat())
+              );
+            },
           )
         ],
       )
     ),
-    
+    );
+  }
+}
+
+class Stat extends StatefulWidget {
+  final Function() notifyParent;
+  Stat({Key key, @required this.notifyParent}) : super(key: key);
+
+  @override
+  StatState createState() => StatState();
+}
+
+class StatState extends State<Stat> {
+  int _childCount = CalculatorState().getButtonCount();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stats',
+        style: TextStyle(fontStyle: FontStyle.italic),
+        ),
+      ),
+      body: Card(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('$_childCount',
+                style: TextStyle(fontSize: 60),),
+              ],
+            ),
+            Card(
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                     _childCount = CalculatorState()._buttonCount; 
+                     print(_childCount);
+                    });
+                  },
+                  child: Text('Refresh'),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
