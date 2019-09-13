7fc866e5b5ed86cedf0f47f21b28e4bc9294097e
BeatMil <anutorn@gmail.com>
2019-09-13 09:35:32 +0700

    put _notitab into listview

--- lib/notification.dart
+++ lib/notification.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class NotificationPage extends StatefulWidget{
+class NotificationPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return NotificationState();
@@ -9,70 +9,54 @@ class NotificationPage extends StatefulWidget{
 }
 
 class NotificationState extends State<NotificationPage> {
-
-  Widget _notificationTab (String imagePath, String messsage, DocumentSnapshot document) {
+  Widget _notificationTab(String imagePath, DocumentSnapshot document) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
         Container(
-          child: Image.asset('assets/MugiQT.png',
-          height: 80,
+          child: Image.asset(
+            'assets/MugiQT.png',
+            height: 80,
           ),
         ),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(document['name'],
-            style: TextStyle(fontSize: 20,
-            fontWeight: FontWeight.bold),),
-            Text(document['message'],
-            style: TextStyle(fontSize: 16),),
+            Text(
+              document['name'],
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              document['message'],
+              style: TextStyle(fontSize: 16),
+            ),
           ],
         )
       ],
     );
   }
-    
-    
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('dotRoot/Notification',
-          style: TextStyle(fontSize: 25,
-          color: Colors.white),
-            ),
-        iconTheme: IconThemeData(color: Colors.white),
-      ),
-      body: StreamBuilder(
-        stream: Firestore.instance.collection('notification').snapshots,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) return const Text('Loading...');
-          return Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.all(20),
-                    child: Text('Clear All',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-              _notificationTab('assets/MugiQT.png', 'how????', snapshot.data.documents[2])
-            ],
-          );
-        }
-      ),
-    );
+        appBar: AppBar(
+          title: Text(
+            'dotRoot/Notification',
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
+        body: StreamBuilder(
+          stream: Firestore.instance.collection('notification').snapshots,
+          builder: (context, snapshots) {
+            if (!snapshots.hasData) return const Text('Loading...');
+            return ListView.builder(
+              itemExtent: 80,
+              itemCount: snapshots.data.documents.length,
+              itemBuilder: (context, index) => _notificationTab(
+                  'assets/MugiQT.png', snapshots.data.documents[index]),
+            );
+          },
+        ));
   }
-
-
-  
-  
 }
